dce27d5adae9faf17ce8440b92a54ec996665cf4
Dragan Mirilo <draganmirilo86@gmail.com>
2023-03-05 20:09:47 +0100

    Add files via upload

---
+++ Number of minutes in hour.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+hours = int(input("Enter the number of hours: "))
+
+minutes = hours % 1 * 60
+
+print(hours, "hour(s) is equal to", minutes, "minute(s)")
+
+
+# In[ ]:
+
+
+# When I enter float number, it shows an error!
+
